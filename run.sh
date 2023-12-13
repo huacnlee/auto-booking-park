0fd219d27840854e6275abcb38496644a6271c07
Jason Lee <huacnlee@gmail.com>
2023-12-13 17:48:37 +0800

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+now=$(echo date +"%T")
+
+cd $HOME/github/auto-booking-park
+
+echo ''
+echo ''
+echo '---------------------------------------------'
+echo "Running navi `$now`"
+echo '---------------------------------------------'
+
+
+echo $(pwd)
+CI=1 $HOME/.navi/navi run main.nv
